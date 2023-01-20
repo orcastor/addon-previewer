40a896331212b2b49caaedf743555b669d6f5f4a
Orca <orca.zhang@yahoo.com>
2023-01-20 14:43:42 +0800

    :maple_leaf: instead fonts by installation

--- back/x2t/common/allfontsgen.sh
+++ back/x2t/common/allfontsgen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-apt-get install fontconfig ttf-mscorefonts-installer fonts-dejavu fonts-liberation fonts-crosextra-carlito fonts-takao-gothic fonts-opensymbol fonts-beng-extra fonts-gujr-extra fonts-telu-extra -y --no-install-recommends
+apt-get install fontconfig ttf-mscorefonts-installer fonts-dejavu fonts-liberation fonts-crosextra-carlito fonts-takao-gothic fonts-opensymbol fonts-beng-extra fonts-gujr-extra fonts-telu-extra fonts-nanum fonts-noto fonts-wqy-zenhei fonts-arphic-ukai fonts-freefont-ttf -y --no-install-recommends
 fc-cache -fv
 
 #Start generate AllFonts.js, font thumbnails and font_selection.bin
