04533e52258309dc9fda610ab0244924610fbce2
orca-zhang <orca.zhang@yahoo.com>
2024-01-07 21:10:40 +0800

    :sunny: chore: minify

--- back/x2t/common/allfontsgen.sh
+++ back/x2t/common/allfontsgen.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/sh
 
-# if there's 'Unable to locate package' errors, use command below to add source for apt
-#echo "deb http://mirrors.aliyun.com/ubuntu/ vivid main restricted universe multiverse
-#deb http://mirrors.aliyun.com/ubuntu/ vivid-security main restricted universe multiverse
-#deb http://mirrors.aliyun.com/ubuntu/ vivid-updates main restricted universe multiverse
-#deb http://mirrors.aliyun.com/ubuntu/ vivid-proposed main restricted universe multiverse
-#deb http://mirrors.aliyun.com/ubuntu/ vivid-backports main restricted universe multiverse
-#deb-src http://mirrors.aliyun.com/ubuntu/ vivid main restricted universe multiverse
-#deb-src http://mirrors.aliyun.com/ubuntu/ vivid-security main restricted universe multiverse
-#deb-src http://mirrors.aliyun.com/ubuntu/ vivid-updates main restricted universe multiverse
-#deb-src http://mirrors.aliyun.com/ubuntu/ vivid-proposed main restricted universe multiverse
-#deb-src http://mirrors.aliyun.com/ubuntu/ vivid-backports main restricted universe multiverse" >> /etc/apt/sources.list
+# if there's an 'Unable to locate package' error, change /etc/apt/sources.list and update
 #apt-get update
 
-apt-get install fontconfig fonts-dejavu fonts-liberation fonts-crosextra-carlito fonts-takao-gothic fonts-opensymbol fonts-beng-extra fonts-gujr-extra fonts-telu-extra fonts-nanum fonts-noto fonts-wqy-zenhei fonts-arphic-ukai fonts-freefont-ttf fonts-takao-gothic -y --no-install-recommends
+apt-get install fontconfig fonts-dejavu fonts-liberation fonts-crosextra-carlito fonts-opensymbol fonts-noto-core fonts-freefont-ttf fonts-droid-fallback fonts-open-sans fonts-symbola ttf-mscorefonts-installer -y --no-install-recommends
 fc-cache -fv
 
 #Start generate AllFonts.js, font thumbnails and font_selection.bin
 echo Generating AllFonts.js, please wait...
 
+mv -f "./libgraphics.so" "./libgraphics.so.bak"
+
+"./upx" -d "./libgraphics.so.bak" -o "./libgraphics.so" > /dev/null 2>&1
+
 "./allfontsgen"\
   --input="`pwd`/core-fonts"\
   --allfonts="./js/AllFonts.js"\
-  --images="./js/sdkjs/common/Images"\
   --selection="./font_selection.bin"\
   --use-system="true"
 
-echo Done
+#--images="./js/sdkjs/common/Images"
+
+mv -f "./libgraphics.so.bak" "./libgraphics.so"
+
+echo Done.
